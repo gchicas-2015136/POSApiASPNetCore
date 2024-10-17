0e8afda509856961bd3de6b042c133801d00fc1b
gchicas-2015136 <giancarlochicas0@gmail.com>
2024-10-17 12:59:41 -0600

    Limpieza de cache y archivos no funcionales

--- POS/POS.Domain/obj/Debug/net8.0/POS.Domain.AssemblyInfo.cs
+++ POS/POS.Domain/obj/Debug/net8.0/POS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c2f523af3fa0ecf515ced4ad5e8f640a4336f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
