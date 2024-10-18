94d92ba810325d18d1fe3f1899ff4a579a172898
gchicas-2015136 <giancarlochicas0@gmail.com>
2024-10-18 09:44:20 -0600

    Corrección de autentificación y autorización para el token JWT en los endpoints de categoría

---
+++ POS/POS.Api/Extensions/SwaggerExtension.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace POS.Api.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            var openApi = new OpenApiInfo
+            {
+                Title = "POS API",
+                Version = "v1",
+                Description = "Punto de venta API",
+                TermsOfService = new Uri("https://opensource.org/licences/NIT"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Giancarlo Chicas",
+                    Email = "Giancarlochicas0@gmail.com",
+                    Url = new Uri("https://opensource.org/licences/NIT")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "License",
+                    Url = new Uri("https://opensource.org/licences/NIT")
+                }
+            };
+
+            services.AddSwaggerGen(x =>
+            {
+                openApi.Version = "v1";
+                x.SwaggerDoc("v1", openApi);
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "JWT Bearer Token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                x.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[]{ } }
+                });
+            });
+
+            return services;
+        }
+    }
+}
